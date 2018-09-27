3591e62eb5d2132b2f08dcf2a2e259b7098f225c
Melody Yu <x864y008@ku.edu>
2018-09-27 14:11:58 -0500

    Initial commit for Quiz.php

--- Exercise2/Quiz.php
+++ Exercise2/Quiz.php
@@ -1,9 +1,9 @@
 <?php
-  $Q1 = $_POST["Q1"];
-  $Q2 = $_POST["Q2"];
-  $Q3 = $_POST["Q3"];
-  $Q4 = $_POST["Q4"];
-  $Q5 = $_POST["Q5"];
+  $Q1 = $_POST['Q1'];
+  $Q2 = $_POST['Q2'];
+  $Q3 = $_POST['Q3'];
+  $Q4 = $_POST['Q4'];
+  $Q5 = $_POST['Q5'];
 
   $correct = 0;
 
@@ -14,39 +14,44 @@
   if($Q5 == "Jordan Spieth") { $correct++; }
 ?>
 
+
 <html>
   <head>
     <title> Quiz Result </title>
   </head>
 
-  <body>
-    <div>
-      Results:
+  <body style="background-color:rgb(214, 214, 194); padding-left:25px">
+    <div style="font-family:arial; font-size: 25pt; font-weight:700px; line-height:50pt">
+      Quiz Results: <br>
     </div>
-
-    <?php
-      echo "Question1: How many holes is a full round of golf ?".'\n';
-      echo "You answered: ".$Q1.'\n';
-      echo "Correct answer: 18".'\n';
-      echo PHP_EOL;
-      echo "Question2: What is a birdie in a Par 3 ?".'\n';
-      echo "You answered: ".$Q2.'\n';
-      echo "Correct answer: 2".'\n';
-      echo PHP_EOL;
-      echo "Question3: What is a bogey in a Par 5 ?".'\n';
-      echo "You answered: ".$Q3.'\n';
-      echo "Correct answer: 6".'\n';
-      echo PHP_EOL;
-      echo "Question4: How many tournaments has Tiger Woods won on the PGA tour so far ?".'\n';
-      echo "You answered: ".$Q4.'\n';
-      echo "Correct answer: 80".'\n';
-      echo PHP_EOL;
-      echo "Question5: Guess who is my favourite player on the tour: ".'\n';
-      echo "You answered: ".$Q5.'\n';
-      echo "Correct answer: Jordan Spieth".'\n';
-      echo PHP_EOL;
-      echo "You got ".$correct." answered correctly!";
-      echo "Score percentage: ".$correct*20."%";
-     ?>
+    <div style="font-family:verdana; line-height:20pt">
+      <?php
+        echo "Question1: How many holes is a full round of golf ?".'<br>';
+        echo "You answered: ".$Q1.'<br>';
+        echo "Correct answer: 18".'<br>';
+        echo '<br>';
+        echo "Question2: What is a birdie in a Par 3 ?".'<br>';
+        echo "You answered: ".$Q2.'<br>';
+        echo "Correct answer: 2".'<br>';
+        echo '<br>';
+        echo "Question3: What is a bogey in a Par 5 ?".'<br>';
+        echo "You answered: ".$Q3.'<br>';
+        echo "Correct answer: 6".'<br>';
+        echo '<br>';
+        echo "Question4: How many tournaments has Tiger Woods won on the PGA tour so far ?".'<br>';
+        echo "You answered: ".$Q4.'<br>';
+        echo "Correct answer: 80".'<br>';
+        echo '<br>';
+        echo "Question5: Guess who is my favourite player on the tour: ".'<br>';
+        echo "You answered: ".$Q5.'<br>';
+        echo "Correct answer: Jordan Spieth".'<br>';
+        echo '<br>';
+        echo "You got ".$correct." answered correctly!".'<br>';
+        echo "Score percentage: ".($correct*20)."%";
+      ?>
+    </div>
+    <br>
+    <a href="Quiz.html" class="button">Take Quiz Again</a> &nbsp&nbsp&nbsp&nbsp
+    <a href="index.html" class="button">Back To Index Page</a><br>
   </body>
 </html>
