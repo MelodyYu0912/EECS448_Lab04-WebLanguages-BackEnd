756fdc428eba26be6b9508442b7e1588a75f570a
Melody Yu <x864y008@ku.edu>
2018-09-27 19:34:29 -0500

    Adjust the layout of the table

--- Exercise1/multiplicationTable.php
+++ Exercise1/multiplicationTable.php
@@ -1,11 +1,36 @@
-<?php
-  for ($i=1; $i<101; $i++)
-  {
-    for ($j=1; $j<101; $j++)
-    {
-      echo $i*$j." ";
-    }
-    echo'<br>';
-  }
+<html>
+  <head>
+    <title> Multiplication Table </title>
+  <head>
+  <body>
+    <table>
+      <?php
+        for ($i=0; $i<101; $i++)
+        {
+          echo "<tr>";
+          if($i != 0)
+          {
+            echo "<td>$i</td>";
+          }
+          else
+          {
+            echo "<td></td>";
+          }
 
-?>
+          for ($j=1; $j<101; $j++)
+          {
+            if($i == 0)
+            {
+              echo "<td>$j</td>";
+            }
+            else
+            {
+              echo "<td>".($i*$j)."</td>";
+            }
+          }
+          echo"<tr>";
+        }
+      ?>
+    </table>
+  </body>
+</html>
